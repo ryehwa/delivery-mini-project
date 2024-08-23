dfc008081c8da6ecf1c06a2e7b089e17353d444c
ryehwa <rhk415@gmail.com>
2024-08-23 11:00:37 +0900

    first commit

---
+++ src/test/java/com/lucky_vicky/delivery_project/DeliveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky_vicky.delivery_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
