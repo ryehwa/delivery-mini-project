7a22a1b63a033ac868131566598d02e79761031e
jun6292 <c68254@gmail.com>
2024-09-01 23:13:35 +0900

    Fix: request dto 입력 값 검증 추가

--- src/main/java/com/lucky_vicky/delivery_project/store/dto/UpdateStoreRequestDto.java
+++ src/main/java/com/lucky_vicky/delivery_project/store/dto/UpdateStoreRequestDto.java
@@ -1,12 +1,14 @@
 package com.lucky_vicky.delivery_project.store.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 import java.util.UUID;
 
 public record UpdateStoreRequestDto(
-        String name,
-        String address,
-        String number,
+        @NotBlank String name,
+        @NotBlank String address,
+        @NotBlank String number,
         List<UUID> categoryIds
 ) {
 }
